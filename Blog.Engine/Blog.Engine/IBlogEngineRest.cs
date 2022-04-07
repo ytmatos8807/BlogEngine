d04bb7d02d9b6f1ac88b531e4edffb0f830c17f4
ytmatos8807 <ytmatos8807@gmail.com>
2022-04-07 10:15:35 -0400

    initial commit
    project structure

---
+++ Blog.Engine/Blog.Engine/IBlogEngineRest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Blog.Engine
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBlogEngineRest" in both code and config file together.
+    [ServiceContract]
+    public interface IBlogEngineRest
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
